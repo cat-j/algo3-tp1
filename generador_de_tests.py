a070bc2bd6c577f1bb2ff9bbfa8687e7f17fc9bb
Cat <catalinajuarros@gmail.com>
2017-08-26 20:59:42 -0300

    Hice la función para generar tests, empecé la sección 2 del informe, terminé las funciones auxiliares, documenté el código

---
+++ generador_de_tests.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def generarCaso(k, umbral_respuesta_positiva=0.5, umbral_responder=0.5):
+    a = 0
+    caso = []
+    
+    for i in range(1,k+1):
+        for j in range(1,k+1):
+            p = np.random.uniform()
+
+            if p >= umbral_responder:
+                a = a + 1
+                v = np.random.uniform()
+                if v >= umbral_respuesta_positiva:
+                    signo = 1
+                else:
+                    signo = -1
+                    j = j * signo
+                caso.append(str(i) + " " + str(j))
+
+    caso.insert(0, str(k) + " " + str(a))
+    return caso
+
+def generarEntrada(hasta, umbral_respuesta_positiva=0.5, umbral_responder=0.5):
+    for i in range(1,hasta+1):
+        caso = generarCaso(i, umbral_respuesta_positiva, umbral_responder)
+        caso.append("0 0")
+        
+        for j in caso:
+            print j
+
+    return
+
+generarEntrada(4, 0.9, 0.1)
--- pruebachota.cpp
+++ pruebachota.cpp
@@ -4,28 +4,53 @@
 using namespace std;
 
 class LicSilverstein {
+
+    /* Esta clase encapsula todas las variables necesarias para resolver el problema planteado:
+    las preguntas respondidas, la cantidad de agentes, el tamaño de la mejor solución encontrada
+    y la solución parcial que se está probando. Los conjuntos representan con la clase std::set, que
+    está implementada con un red-black tree, para asegurar un orden de complejidad logarítmico en las
+    búsquedas, inserciones y borrados de los métodos auxiliares. */
     
     public:
-        LicSilverstein(int i, int a, set<pair<int,int> > e) : cantAgentes(i), cantEncuestas(a), encuestas(e) {}
+        LicSilverstein(int i, int a, set<pair<int,int> > e, bool p) : cantAgentes(i), cantEncuestas(a), encuestas(e) {}
         ~LicSilverstein();
 
         int mayorCantidad() {
+            // Método principal.
             return mayorCantidadAux(1);
         }
 
         int mayorCantidadAux(const int n) {
+
+            /* Método auxiliar que extiende a mayorCantidad con un parámetro n sobre el que hace recursión.
+            En cada llamada, n representa el número de agente sobre el que tomamos una decisión;
+            crece hasta que no quedan más agentes para agregar.
+            Complejidad: O(2**cantAgentes * cantAgentes * log(cantAgentes) * cantEncuestas) */
+
             if (n==cantAgentes) {
+
+                // Caso base: n es el último agente
+
                 if (esValidoAgregar(n)) { solucionParcial.insert(n); }
                 maxCantidadEncontrada = solucionParcial.size();
-            	return solucionParcial.size();
+                return solucionParcial.size();
+
             } else {
+
+                /* Caso recursivo: elegimos la máxima cantidad a la que se puede llegar en cada subárbol
+                válido (agregar a n o no agregar a n). */
+
                 int sinAgregar = 0;
                 int agregando = 0;
+
+                /* Los siguientes ifs aseguran que esta recursión se corresponde con un árbol de backtracking y
+                no con uno de fuerza bruta, puesto que las llamadas recursivas no se efectúan cuando una decisión
+                resultaría en una solución inválida que no puede extenderse a una válida. */
 
                 if (esValidoAgregar(n)) {
                     solucionParcial.insert(n);
                     agregando = mayorCantidadAux(n+1);
-                    solucionParcial.erase(n);
+                    solucionParcial.erase(n); // vuelve para atrás: el "backtrack" en backtracking
                 }
 
                 if (esValidoNoAgregar(n)) {
@@ -37,6 +62,15 @@ class LicSilverstein {
         }
 
         bool esValidoAgregar(int n) {
+
+            /* Verifica si es posible extender solucionParcial a una solución válida agregando a n.
+            Es importante destacar que esta función NO DICE si solucionParcial unión n es válida por sí sola,
+            sino que verifica que ningún agente ya agregado diga que el agente n no es confiable o que n
+            diga que algún agente ya agregado no es confiable.
+            Si esValidoAgregar verificara la validez de esa solución, sería imposible extender un conjunto
+            que es válido como solución parcial a uno que es válido como solución candidata, no se exploraría
+            todo el espacio de soluciones y el algoritmo no resolvería el problema. */
+
             int j = 1;
             while(j < solucionParcial.size()) {
                 if (noConfiableSegunJ(n,j)) { return false; }
@@ -46,6 +80,14 @@ class LicSilverstein {
         }
 
         bool esValidoNoAgregar(int n) {
+
+            /* Similar a la función anterior. Si algún agente ya agregado dice que n es confiable, devuelve
+            False, puesto que sería obligatorio agregar a n dada esa información. */
+
+            int j = 1;
+            while(j < solucionParcial.size()) {
+                if (confiableSegunJ(n,j)) { return false; }
+            }
         }
 
         bool noConfiableSegunJ(int n, int j) {
@@ -57,12 +99,22 @@ class LicSilverstein {
             return true;
         }
 
+        bool confiableSegunJ(int n, int j) {
+            for (auto pregunta = encuestas.begin(); pregunta != encuestas.end(); ++pregunta) {
+                if ( pregunta->first == j && pregunta->second == n ) { return false; }
+            }
+
+            return true;
+        }
+
     private:
-        set<pair<int,int> > encuestas;
-        int cantAgentes;
-        int cantEncuestas;
-        int maxCantidadEncontrada;
+        set<pair<int,int> > encuestas; //conjunto de preguntas respondidas
+        int cantAgentes; //tamaño del conjunto de agentes
+        int cantEncuestas; //tamaño del conjunto de preguntas respondidas; variable auxiliar para hacer más declarativo el código
+        int maxCantidadEncontrada; //tamaño de la mejor solución hallada; variable auxiliar para hacer las podas
         set<int> solucionParcial;
+        /* esta variable va mutando durante la ejecución del algoritmo; su estado representa
+        el nodo del árbol en el que estamos parados */
 };
 
 int main() {
